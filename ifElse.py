9e142d41f85a204f2ae8e69ba1bcc4b7e78752aa
Suman Pathak <50736574+sumanpathak14@users.noreply.github.com>
2020-06-16 12:29:48 +0530

    Create ifElse.py

---
+++ ifElse.py
@@ -0,0 +1,13 @@
+if __name__ == '__main__':
+    n = int(input())
+    
+w = 'Weird'
+nw = 'Not Weird'
+if n % 2 == 1:
+    print(w)
+elif n % 2 == 0 and (n>=2 and n<5):
+    print(nw)
+elif n % 2 == 0 and (n>=6 and n<=20):
+    print(w)
+elif n % 2 == 0 and (n>20):
+    print(nw) 
